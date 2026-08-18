c59608591a515f92cb0d4fc5f47448c1b450423a
Rayhan Mohamed <rayhan24k@gmail.com>
2026-08-17 22:15:29 -0400

    added new project and swapped orders

--- src/components/sections/Projects..jsx
+++ src/components/sections/Projects..jsx
@@ -10,65 +10,34 @@ export const Projects = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> Flappy Goose Game </h3>
+                    <h3 className="text-xl font-bold mb-2"> Leafs Wordle </h3>
                     <p className="text-gray-400 mb-4">
-                        Flappy Bird Clone game made in Unity.
+                        Wordle style game for all times leafs players to increase my puck knowledge.
                     </p>
                     <div className="flex flex-wrap gap-1">
-                        {["Unity", "C#"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
+                        {["Next.js", "React","Python"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
                                     {tech}
                                 </span>))}
                     </div>
                     <div className="flex justify-between items-center">
-                        <a href="https://github.com/Overrayted/flappybird-clone" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
-                    </div>
-                </div>
-
-                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> Chat App </h3>
-                    <p className="text-gray-400 mb-4">
-                        Chat application with real time messaging, chat creation and google authentication.
-                    </p>
-                    <div className="flex flex-wrap gap-1">
-                        {["Next.js", "React","Firebase"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
-                                    {tech}
-                                </span>))}
-                    </div>
-                    <div className="flex justify-between items-center">
-                        <a href="https://whatsapp-clone-90108.web.app/" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
+                        <a href="https://leafdle.ca" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
                     </div>
                 </div>
 
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> ProjectF </h3>
+                    <h3 className="text-xl font-bold mb-2"> PocketTypes </h3>
                     <p className="text-gray-400 mb-4">
-                        Onboard flight computer using arduino nano.
+                        Pokemon styled typing game to help kids get better at typing.
                     </p>
                     <div className="flex flex-wrap gap-1">
-                        {["C++"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
+                        {["Java","JavaFX"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
                                     {tech}
                                 </span>))}
                     </div>
                     <div className="flex justify-between items-center">
-                        <a href="https://github.com/Overrayted/projectF" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
+                    <a href="https://github.com/Overrayted/PocketTypes" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
                     </div>
                 </div>
-
-                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> Personal Portfolio </h3>
-                    <p className="text-gray-400 mb-4">
-                        The website your looking at right now!
-                    </p>
-                    <div className="flex flex-wrap gap-1">
-                        {["Javascript","React", "TailwindCSS"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
-                                    {tech}
-                                </span>))}
-                    </div>
-                    <div className="flex justify-between items-center">
-                        <a href="#home" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
-                    </div>
-                </div>
-
 
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
                     <h3 className="text-xl font-bold mb-2"> Scholarship App </h3>
@@ -86,17 +55,49 @@ export const Projects = () => {
                 </div>
 
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
-                    <h3 className="text-xl font-bold mb-2"> PocketTypes </h3>
+                    <h3 className="text-xl font-bold mb-2"> Personal Portfolio </h3>
                     <p className="text-gray-400 mb-4">
-                        Pokemon styled typing game to help kids get better at typing.
+                        The website your looking at right now!
                     </p>
                     <div className="flex flex-wrap gap-1">
-                        {["Java","JavaFX"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
+                        {["Javascript","React", "TailwindCSS"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
                                     {tech}
                                 </span>))}
                     </div>
                     <div className="flex justify-between items-center">
-                    <a href="https://github.com/Overrayted/PocketTypes" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
+                        <a href="#home" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
+                    </div>
+                </div>
+
+    
+
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
+                    <h3 className="text-xl font-bold mb-2"> Chat App </h3>
+                    <p className="text-gray-400 mb-4">
+                        Chat application with real time messaging, chat creation and google authentication.
+                    </p>
+                    <div className="flex flex-wrap gap-1">
+                        {["Next.js", "React","Firebase"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
+                                    {tech}
+                                </span>))}
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <a href="https://whatsapp-clone-90108.web.app/" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
+                    </div>
+                </div>
+
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2_8px_rgba(255,0,255,0.2)] transition">
+                    <h3 className="text-xl font-bold mb-2"> Flappy Goose Game </h3>
+                    <p className="text-gray-400 mb-4">
+                        Flappy Bird Clone game made in Unity.
+                    </p>
+                    <div className="flex flex-wrap gap-1">
+                        {["Unity", "C#"].map((tech, key) => (<span key={key} className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 hover:shadow-[0_2_8px_rgba(255,0,255,0.1)] transition-all">
+                                    {tech}
+                                </span>))}
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <a href="https://github.com/Overrayted/flappybird-clone" target="_blank" rel="noopener noreferrer" className="text-purple-400 hover:text-purple-300 transition-colors my-4"> View Project →</a>
                     </div>
                 </div>
 
